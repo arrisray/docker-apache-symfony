675ad91a04165f18c095de02de971b9d2da4e7b8
Mancobian Poemandres <mancobian.poemandres@gmail.com>
2015-07-30 17:17:38 -0400

    .

--- start.sh
+++ start.sh
@@ -3,5 +3,8 @@
 chown -R www-data: /var/www/askeet
 chmod -R a+rX /var/www/askeet
 
+chown -R www-data: /usr/share/php/data
+chmod -R a+rX /usr/share/php/data
+
 source /etc/apache2/envvars
 exec apache2 -D FOREGROUND
