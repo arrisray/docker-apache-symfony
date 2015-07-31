53e181bc74e6d5cb28d1cc1078af95e4f138ce6f
Mancobian Poemandres <mancobian.poemandres@gmail.com>
2015-07-31 13:00:56 -0400

    .

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ if [[ ! -d $VOLUME_HOME/mysql ]]; then
     echo "=> Installing MySQL ..."
     mysql_install_db > /dev/null 2>&1
     echo "=> Done!"  
-    /create_mysql_admin_user.sh
+    /opt/create_mysql_admin_user.sh
 else
     echo "=> Using an existing volume of MySQL"
 fi
